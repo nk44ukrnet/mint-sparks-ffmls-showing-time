a9da6e9fac14b5c378ec19aea5ce7f938b9c6e50
nk44ukrnet <56731417+nk44ukrnet@users.noreply.github.com>
2022-11-10 11:29:24 +0200

    q

--- page-templates/realtor-shceduled-showings.php
+++ page-templates/realtor-shceduled-showings.php
@@ -85,7 +85,7 @@ get_header();
 
                     <div class="app-main-content__holder">
 
-                        <div class="app-panel app-panel_green active">
+                        <div class="app-panel app-panel_green app-panel-scheduled-showings">
                             <div class="app-panel__head panel-head expand-head">
                                 <div class="panel-head__text">
                                     <h2 class="panel-head__heading">
@@ -99,59 +99,63 @@ get_header();
                                     <span></span>
                                 </div>
                             </div>
-                            <div class="app-panel__content panel-content-type-realtor ">
-                                <div class="panel-content-type-realtor__top">
+                            <div class="app-panel__content panel-content-type-scheduled-showings">
+                                <div class="panel-content-type-scheduled-showings__top">
                                     <div class="app-property-details-table_inner">
-                                        <div class="panel-content-type-realtor__col">
+                                        <div class="panel-content-type-scheduled-showings__col">
                                             <img src="<?php echo get_template_directory_uri(); ?>/assets/img/img-placeholder.png"
-                                                 alt="image" loading="lazy" class="panel-content-type-realtor__img">
-                                            <div class="panel-content-type-realtor__row">
-                                                <div class="panel-content-type-realtor__title">Listing Price</div>
-                                                <div class="panel-content-type-realtor__value"> $229,900</div>
+                                                 alt="image" loading="lazy"
+                                                 class="panel-content-type-scheduled-showings__img">
+                                            <div class="panel-content-type-scheduled-showings__row">
+                                                <div class="panel-content-type-scheduled-showings__title">Listing
+                                                    Price
+                                                </div>
+                                                <div class="panel-content-type-scheduled-showings__value">$229,900</div>
                                             </div>
                                         </div>
-                                        <div class="panel-content-type-realtor__col">
-                                            <div class="panel-content-type-realtor__row">
-                                                <div class="panel-content-type-realtor__title">Style:</div>
-                                                <div class="panel-content-type-realtor__value"> Single Family
-                                                    Residence
+                                        <div class="panel-content-type-scheduled-showings__col">
+                                            <div class="panel-content-type-scheduled-showings__row">
+                                                <div class="panel-content-type-scheduled-showings__title">
+                                                    Scheduled Showing date:
+                                                </div>
+                                                <div class="panel-content-type-scheduled-showings__value">
+                                                    07.16.2022
                                                 </div>
                                             </div>
-                                            <div class="panel-content-type-realtor__row">
-                                                <div class="panel-content-type-realtor__title">County:</div>
-                                                <div class="panel-content-type-realtor__value"> Pinellas Country</div>
+                                            <div class="panel-content-type-scheduled-showings__row">
+                                                <div class="panel-content-type-scheduled-showings__title">
+                                                    Scheduled Showing time:
+                                                </div>
+                                                <div class="panel-content-type-scheduled-showings__value">
+                                                    7:30 AM
+                                                </div>
                                             </div>
-                                            <div class="panel-content-type-realtor__row">
-                                                <div class="panel-content-type-realtor__title">Bedrooms:</div>
-                                                <div class="panel-content-type-realtor__value"> 2</div>
-                                            </div>
-                                            <div class="panel-content-type-realtor__row">
-                                                <div class="panel-content-type-realtor__title">Full Baths:</div>
-                                                <div class="panel-content-type-realtor__value"> 2</div>
+                                            <div class="panel-content-type-scheduled-showings__row">
+                                                <div class="panel-content-type-scheduled-showings__title">
+                                                    Showing request status:
+                                                </div>
+                                                <div class="panel-content-type-scheduled-showings__value">
+                                                    date and time reviewed by the seller
+                                                </div>
                                             </div>
                                         </div>
-                                        <div class="panel-content-type-realtor__col">
-                                            <div class="panel-content-type-realtor__row">
-                                                <div class="panel-content-type-realtor__title">Half Baths:</div>
-                                                <div class="panel-content-type-realtor__value"> 0</div>
-                                            </div>
-                                            <div class="panel-content-type-realtor__row">
-                                                <div class="panel-content-type-realtor__title">Square Feet:</div>
-                                                <div class="panel-content-type-realtor__value"> 2520</div>
-                                            </div>
-                                            <div class="panel-content-type-realtor__row">
-                                                <div class="panel-content-type-realtor__title">Private Pool:</div>
-                                                <div class="panel-content-type-realtor__value"> YES</div>
-                                            </div>
-                                            <div class="panel-content-type-realtor__row">
-                                                <div class="panel-content-type-realtor__title">Garage:</div>
-                                                <div class="panel-content-type-realtor__value"> YES</div>
+                                    </div>
+                                    <div class="app-panel-event event">
+                                        <a href="#!" class="event__head">Event Log <em></em></a>
+                                        <div class="event__content">
+                                            <div class="event__item event__item_border-bottom">
+                                                <div class="event__title">
+                                                    Showing requested by <a href="mailto:realtor@brokerage.com">realtor@brokerage.com</a>
+                                                </div>
+                                                <div class="event__value">
+                                                    07.12.2022
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="panel-footer">
-                                    <a href="#!" class="app-btn app-btn_green">Schedule Your Showing</a>
+                                <div class="panel-footer panel-content-type-scheduled-showings__bottom">
+                                    <a href="#!" class="app-btn app-btn_green">Cancel Showing</a>
                                 </div>
                             </div>
                         </div>
